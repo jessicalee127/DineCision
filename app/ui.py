06db0ef2343a91b08d5ad37f30cdadc27125214b
Jiarong (Jessica) Li <jessicalee127@gmail.com>
2018-06-23 22:48:04 -0400

    Added error handling
    Display proper error when
    - no input from user
    - not enough restaurant data

--- app/ui.py
+++ app/ui.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash, redirect, url_for, session
+from flask import Flask, request, render_template, flash, redirect, url_for
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from wtforms.validators import DataRequired
 from app.DineCision import yelprequest
@@ -7,18 +7,13 @@ import random
 import json
 import os
 
-API_KEY = os.environ["DINECISION_API_KEY"]
+API_KEY = os.environ.get("DINECISION_API_KEY") or "Please obtain a Yelp API Key and set it as an environment variable named 'DINECISION_API_KEY'"
 SECRET_KEY = os.environ.get("SECRET_KEY") or "my super secret"
 
 # API constants, you shouldn't have to change these.
 API_HOST = 'https://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
-BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
-
-# Defaults for our simple example.
-DEFAULT_TERM = 'dinner'
-DEFAULT_LOCATION = 'New York City, NY'
-SEARCH_LIMIT = 10
+BUSINESS_PATH = '/v3/businesses/'
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
@@ -30,19 +25,27 @@ class NameForm(Form):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     location = None
+    form = NameForm(request.form)
     if request.method == 'POST' and 'location' in request.form:
-        location = request.form['location']
-        return redirect(url_for('confirm', location=request.form.get('location')))
-        # if form.validate():
-        #     flash('Your entered: ' + location)
-        # else:
-        #     flash('Location is a required field')
+        if form.validate():
+            return redirect(url_for('confirm', location=request.form.get('location')))
+        else:
+            flash("Please enter a location")
+            return redirect(url_for('error'))
     return render_template('index.html')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
 
 @app.route('/confirm/<location>')
 def confirm(location):
     random_business = yelp(location)
-    return render_template('confirm.html', random_business=random_business['name'], where_is_it_0=random_business["location"]['display_address'][0], where_is_it_1=random_business["location"]['display_address'][1], number_review=random_business["review_count"], pic_url=random_business["image_url"])
+    if random_business is None:
+        flash("Sadly there is no good restaurant to recommend in this location due to limited data, please choose another location")
+        return redirect(url_for('error'))
+    else:
+        return render_template('confirm.html', random_business=random_business['name'], where_is_it_0=random_business["location"]['display_address'][0], where_is_it_1=random_business["location"]['display_address'][1], number_review=random_business["review_count"], pic_url=random_business["image_url"])
 
 
 def yelp(location_input):
@@ -52,16 +55,18 @@ def yelp(location_input):
         'radius': 500,
         'is_closed': "false",
         'rating': 4,
-        'limit': SEARCH_LIMIT,
+        'limit': 5,
         'categories': "restaurants, All",
         'price': 2
     }
 
-    recommendation = []
     result = yelprequest(API_HOST, SEARCH_PATH, API_KEY, url_params)
     business_list = result["businesses"]
-    random_business = random.choice(business_list)
-    return random_business
+    try:
+        random_business = random.choice(business_list)
+        return random_business
+    except IndexError:
+        return None
 
 
 if __name__ == "__main__":
